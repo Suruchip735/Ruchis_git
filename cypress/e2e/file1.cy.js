dddf113acf116f3bc07ee7bf929184efbfa37642
Suruchip735 <suruchis@zignuts.com>
2025-08-28 16:55:15 +0530

    Add webtables features

---
+++ cypress/e2e/file1.cy.js
@@ -0,0 +1,60 @@
+describe('template spec', () => {
+  it('Input fields', () => {
+    //cy.visit('https://example.cypress.io')
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+
+    cy.get('#inputEmail1').type('Hello Ruchi')
+    .press(Cypress.Keyboard.Keys.TAB) // same as pressing tab key 
+  }),
+
+  it('Checkboxes',() => {
+    cy.visit('/')
+    cy.contains('Modal & Overlays').click()
+    cy.contains('Toastr').click()
+    // --Case 1: Check all checkboxes isible 
+    cy.get('[type="checkbox"]').check({force:true})
+
+    // ---Case 2: Uncheck all checkboxes visible 
+        cy.get('[type="checkbox"]').uncheck({force:true})
+  }),
+
+  it('Tooltips',() => {
+    cy.visit('/')
+    cy.contains('Modal & Overlays').click()
+    cy.contains('Tooltip').click()
+    cy.contains('button','Top').trigger('mouseenter')
+    cy.get('nb-tooltip').should('have.text','This is a tooltip')
+  }),
+  it('Dialog Boxes',() =>{
+    cy.visit('/')
+    cy.contains('Tables & Data').click()
+    cy.contains('Smart Table').click()
+    cy.get('.nb-trash').first().click() 
+
+  // Clicking on dialog box 
+  // Method 1 
+  //1.
+    cy.get('.nb-trash').first().click()
+    cy.on('window:confirm', confirm => {
+        expect(confirm).to.equal('Are you sure you want to delete?')
+    })
+
+    //Method 2
+    cy.window().then( win => {
+        cy.stub(win, 'confirm').as('dialogBox').returns(false)
+    })
+    cy.get('.nb-trash').first().click()
+    cy.get('@dialogBox').should('be.calledWith', 'Are you sure you want to delete?')
+
+
+  })
+
+  it.only('Web tables -1',() => {
+    cy.visit('/')
+    cy.contains('Tables & Data').click()
+    cy.contains('Smart Table').click() 
+  })
+})
+
